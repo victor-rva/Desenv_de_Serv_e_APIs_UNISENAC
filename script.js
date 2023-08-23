fff917e3888032eff9ae901b6c9e8160f9174c57
Victor Vecchio <victorvecchio.rva@gmail.com>
2023-08-22 22:56:36 -0300

    Ajeitando pasta da aula03

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function(){
-        $("#minhaDiv").css("width", "200px");
-        $("#minhaDiv").css("height", "300px");
-        $("#minhaDiv").css("background-color", "#f0f");
-        $("#minhaDiv").css("color", "white");
-        $("#minhaDiv").html("<i>Olá</>");
-        $("#minhaDiv").text("<i>Pessoal</>");
-    }
-);
