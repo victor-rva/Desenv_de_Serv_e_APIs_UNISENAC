ecdeb12e1af85fba4c6fc6c5544f22cc122100ad
Victor Vecchio <111601821+victor-rva@users.noreply.github.com>
2023-09-05 16:05:42 -0300

    Update servidor.php

--- servidor.php
+++ servidor.php
@@ -1,18 +1,20 @@
-<?php 
+<?php
     header("Content-type: application/json");
-
-    $conn = mysqli_connect("localhost", "root", "", "loja_2023_2");
-    if($conn){
-        $result = mysqli_query($conn, "SELECT * FROM produtos");
-        $linhas = array();
-        while($row = mysqli_fetch_assoc($result)){
-            $linhas[] = $row;
+    try {
+        $conn = mysqli_connect("localhost", "root", "", "loja_2023_2");
+        if( $conn ){
+            $result = mysqli_query($conn, "SELECT * FROM produtos");
+            $linhas = array();
+            while( $row = mysqli_fetch_assoc($result) ){
+                $linhas[] = $row;
+            }
+            mysqli_close($conn);
+            echo '{ "produtos" : '. json_encode($linhas) .' }';
+        }else{
+            echo '{ "resposta" : "Erro ao conectar com o banco de dados" }';
         }
-        mysqli_close($conn);
-        echo '{"produtos" : '. json_encode($linhas) .'}';
-    }else{
-        echo '{"resposta" : "Erro ao conectar com o banco de dados."}';
+    } catch (\Throwable $th) {
+        echo '{ "resposta" : "Erro ao conectar com o banco de dados" }';
     }
-
 
 ?>
