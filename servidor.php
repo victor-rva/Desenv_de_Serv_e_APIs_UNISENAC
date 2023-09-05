710332385b2a3fbefbeb998fbdac39cdfff1173a
Victor Vecchio <111601821+victor-rva@users.noreply.github.com>
2023-09-05 11:51:38 -0300

    Aula05-JSON

---
+++ servidor.php
@@ -0,0 +1,18 @@
+<?php 
+    header("Content-type: application/json");
+
+    $conn = mysqli_connect("localhost", "root", "", "loja_2023_2");
+    if($conn){
+        $result = mysqli_query($conn, "SELECT * FROM produtos");
+        $linhas = array();
+        while($row = mysqli_fetch_assoc($result)){
+            $linhas[] = $row;
+        }
+        mysqli_close($conn);
+        echo '{"produtos" : '. json_encode($linhas) .'}';
+    }else{
+        echo '{"resposta" : "Erro ao conectar com o banco de dados."}';
+    }
+
+
+?>
