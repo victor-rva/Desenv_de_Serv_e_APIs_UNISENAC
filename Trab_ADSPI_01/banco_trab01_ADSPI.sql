f0cc8702798da4663305e09b5f57e7abf3cce91a
VICTOR ROCCA VECCHIO ALMEIDA <632220111@fspoa.br>
2023-10-17 10:44:46 -0300

    Trabalho ADSPI

---
+++ Trab_ADSPI_01/banco_trab01_ADSPI.sql
@@ -0,0 +1,50 @@
+-- MYSQL
+CREATE DATABASE loja_online;
+
+USE loja_online;
+
+CREATE TABLE cidades(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50)
+);
+
+CREATE TABLE clientes (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+nome VARCHAR(100) NOT NULL ,
+altura DOUBLE ,
+nascimento DATE DEFAULT '1980-01-01' ,
+cidade_id INT ,
+FOREIGN KEY (cidade_id) REFERENCES cidades(id)
+);
+
+CREATE TABLE pedidos (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+horario DATETIME ,
+endereco VARCHAR(100) NOT NULL ,
+cliente_id INT ,
+FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE TABLE categorias(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100) 
+);
+
+CREATE TABLE produtos (
+id int NOT NULL primary key auto_increment,
+nome VARCHAR(100),
+preco DOUBLE,
+quantidade DOUBLE,
+categoria_id INT,
+FOREIGN KEY(categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE pedidos_produtos(
+pedido_id INT NOT NULL ,
+produto_id INT NOT NULL ,
+preco DOUBLE,
+quantidade DOUBLE,
+PRIMARY KEY (pedido_id, produto_id),
+foreign key(pedido_id) references pedidos (id),
+foreign key(produto_id) references produtos (id)
+);
