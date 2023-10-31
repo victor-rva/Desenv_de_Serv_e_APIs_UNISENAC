a65c3718c71f47d0963ec12b9fdfb79deb92f21a
VICTOR ROCCA VECCHIO ALMEIDA <632220111@fspoa.br>
2023-10-31 11:46:40 -0300

    Aula11

---
+++ Aula11-Integracao/index.js
@@ -0,0 +1,93 @@
+const restify = require('restify');
+const errors = require('restify-errors');
+
+const corsMidlleware = require('restify-cors-middleware2'); //Para fazer requisição AJAX no front-end precisa utilizar o corsMiddleware
+
+const cors = corsMidlleware({
+    origins: ['*'],
+});
+
+const servidor = restify.createServer({
+    name: 'loja',
+    version: '1.0.0'
+});
+
+servidor.use(restify.plugins.acceptParser(servidor.acceptable));
+servidor.use(restify.plugins.queryParser());
+servidor.use(restify.plugins.bodyParser());
+
+
+
+
+servidor.listen(8001, function() {
+    console.log("%s executando em %s", servidor.name, servidor.url)
+});
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'loja_2023_2'
+    }
+});
+
+servidor.get('/', (req, res, next) => {
+    res.send('Bem-vindo(a) a API Loja!')
+});
+
+servidor.get('/produtos', (req, res, next) => {
+    knex('produtos').then((dados) => {
+        res.send(dados);
+    }, next);
+});
+
+servidor.get('/produtos/:idProd', (req, res, next) => {
+    const idProduto = req.params.idProd;
+    knex('produtos')
+        .where('id', idProduto) //id busca o id do banco
+        .first()
+        .then((dados) => {
+            if (!dados || dados =="") {
+                return res.send(new errors.BadRequestError('Este produto não foi encontrado'));
+            }else{
+                res.send(dados);
+            }
+        }, next);
+});
+
+servidor.post('/produtos', (req, res, next) => {
+    knex('produtos')
+        .insert(req.body)
+        .then((dados) => {
+            res.send(dados);
+        }, next);
+});
+
+
+servidor.put('/produtos/update/:idProd', (req, res, next) => {
+    const idProduto = req.params.id;
+    knex('produtos')
+        .where('id', idProduto)
+        .update(req.body)
+        .then((dados) => {
+            if (!dados) {
+                return res.send(new errors.BadRequestError('Este produto não foi encontrado'));
+            }
+            res.send("Produto Atualizado");
+        }, next);
+});
+
+servidor.del('/produtos/:idProd', (req, res, next) => {
+    const idProduto = req.params.id;
+    knex('produtos')
+        .where('id', idProduto)
+        .delete()
+        .then((dados) => {
+            if (!dados) {
+                return res.send(new errors.BadRequestError('Este produto não foi encontrado'));
+            }
+            res.send("Produto Deletado");
+        }, next);
+});
